c47c0c826e9fb6410c55be30a789321e4695ebd8
mariapojarskaya <pojarskaya.maria@gmail.com>
2018-10-24 15:03:53 +0300

    Initial commit

---
+++ src/test/java/lesson1/SimpleTest.java
@@ -0,0 +1,41 @@
+package lesson1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.System.setProperty;
+import static org.testng.Assert.assertEquals;
+
+public class SimpleTest {
+    @Test
+    public void simpleTest() {
+        setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+
+        //1 Open BR
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //2 Navigate
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //3 Assert Title
+        assertEquals(driver.getTitle(), "Home Page");
+
+        //4 Login
+        driver.findElement(By.cssSelector(".profile-photo")).click();
+        driver.findElement(By.cssSelector("[id = 'Name']")).sendKeys("epam");
+        driver.findElement(By.cssSelector("[id = 'Password']")).sendKeys("1234");
+        driver.findElement(By.cssSelector("[type = 'submit']")).click();
+
+        WebElement mainTitle = driver.findElement(By.cssSelector("h3.main-title"));
+        assertEquals(mainTitle.getText(), "EPAM FRAMEWORK WISHES…");
+
+        driver.close();
+    }
+}
